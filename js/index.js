d898d51867210d74bab711c8bfd100a9af4ed937
Diana Martinez <d.martinezfrias@ugto.mx>
2023-05-08 12:58:36 -0600

    corregi mi error

--- js/index.js
+++ js/index.js
@@ -139,6 +139,26 @@ window.addEventListener("scroll", function() {
     header.classList.toggle("abajo", window.scrollY > 0)
 })
 
+// Funciones para el menu de carrusel
+function App() { }
+window.onload = function (event) {
+    var app = new App()
+    window.app = app
+}
+
+App.prototype.processingButton = function (event) {
+    let leftPosition = 0
+    const btn = event.currentTarget
+    const carruselList = event.currentTarget.parentNode
+    const track = event.currentTarget.parentNode.querySelector('.track')
+    const carrusel = track.querySelectorAll('.carrusel')
+    const carruselWidth = carrusel[0].offsetWidth
+    const trackWidth = track.offsetWidth
+    const listWidth = carruselList.offsetWidth
+    track.style.left == "" ? leftPosition = track.style.left = 0 : leftPosition = parseFloat(track.style.left.slice(0, -2) * -1)
+    btn.dataset.button == "button-prev" ? prevAction(leftPosition, carruselWidth, track) : nextAction(leftPosition, trackWidth, listWidth, carruselWidth, track)
+}
+
 // Funcion que muestra las tarjetas
 /*
 const creaCards = (productos) => {
