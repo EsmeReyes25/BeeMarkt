6469bf8d7f9333385066a9476d7f6d2c463dc236
Esme <123397522+EsmeReyes25@users.noreply.github.com>
2023-05-22 17:26:05 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,20 +1,19 @@
 import { getProducts, saveProduct, deleteProduct } from './connection.js' // Imports the querys to get, save and delete products
 
 // Form to insert a new product 
-const form = document.querySelector(".formulario") // Selects the form
+const form = document.querySelector('.formulario') // Selects the form
 const btnAdd = document.querySelector('.btnAdd') // Selects the button to add products
-const btnClose = document.querySelector('.btn-close') // Selects the button to close the form
+const btnClose = document.querySelector('#btnClose') // Selects the button to close the form
 
 // Modal to delete a product
 const btnDelete = document.querySelector('#btnDelete') // Selects the button to delete the product
 const btnCancel = document.querySelector('#btnCancel') // Selects the button to cancel
 
-
 // Carga de tarjetas
 const cardTop = document.querySelector('#products-top').content
 const contenido = document.querySelector('#contenido-productos')
 const fragment = document.createDocumentFragment()
-const Buscar = document.getElementById('buscador') // ******PENDIENTE******
+const Buscar = document.getElementById('buscador')
 
 
 //Seccion Comida
@@ -36,8 +35,10 @@ let products = [] // Here the products are loaded from the database
 let productDelete = {} // Temporal object to call the deleteProd() function
 
 // Load the document, fetch the elements from the database and saves it in the 'products' list, then starts the main() function called inside loadProducts()
-document.addEventListener('DOMContentLoaded', e => {
-    loadProducts()
+document.addEventListener('DOMContentLoaded', async(e) => {
+    products = await getProducts()
+    console.log('productos:', products)
+    main()
 })
 
 // The main function prevents any method from being executed if the products in the list have not been loaded yet
@@ -47,25 +48,18 @@ const main = () => {
     creaCardsBebidas()
     creaCardsOtros()
     
-     // Aquí van las llamadas a las demás funciones y listeners que necesitan de productos[]
+    // Aquí van las llamadas a las demás funciones y listeners que necesitan de productos[]
     //Función para buscar un producto
     Buscar.addEventListener('keyup', () => {
         //console.log('Si llega aqui')
         let temp = []
         temp = products.filter(producto => producto.product_name.toLowerCase().includes(Buscar.value.toLowerCase()))
-       creaCardsALL(temp)
-       //console.log('temp=>', temp)
+        creaCardsALL(temp)
+        //console.log('temp=>', temp)
     }) 
-}
-    
-    
-    
-const loadProducts = async () => {
-    products = await getProducts()
-    console.log('productos:', products)
-    main()
-}
-
+} 
+   
+// ---------------- Aquí van las definiciones de todas las funciones (y de los listeners que no dependen de productos[]) ------------------
 // Button (inside the modal) to add a product
 btnAdd.addEventListener('click', async() => {
     await insertProduct()
@@ -208,8 +202,6 @@ const creaCardsALL = (productos) => {
     productosAll.appendChild(fragment)
 }
 
-
-
 // Change color effect on header
 window.addEventListener("scroll", function() {
     var header = document.querySelector("header")
@@ -249,18 +241,6 @@ let nextAction = (leftPosition, trackWidth, listWidth, carruselWidth, track) => 
 }
 
 // Function to add a product
-/*const insertProduct = async () => {
-    // Data to be added to the database
-    const sendData = {
-        category: form.categ.value,
-        contact: form.contacto.value,
-        image_url: form.imgUrl.value,
-        price: form.precio.value,
-        product_name: form.productName.value,
-        sale_days: form.dias.value,
-        sale_hours: form.horas.value,
-        vendor_name: form.vendorName.value
-    }*/
 const insertProduct = async () => {
     // Data to be added to the database
     const sendData = {
@@ -272,11 +252,10 @@ const insertProduct = async () => {
         sale_days: form.dias.value,
         sale_hours: form.horas.value,
         vendor_name: form.vendorName.value
-        
     };
     await saveProduct(sendData)
     form.reset()
-
+    btnAdd.disabled = true
 };
 
 
